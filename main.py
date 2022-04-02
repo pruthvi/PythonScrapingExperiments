ac6bfc33a4a5f14a25e1a945bede8976f1528e73
Pruthvi <pruthvi@users.noreply.github.com>
2022-04-02 13:31:54 -0400

    Exports data into CSV and Added Comments

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 
+import pandas as pd
+
 url = 'https://www.drug-inspections.canada.ca/gmp/searchResult-en.html?estName=&ref=&site=&eType=&prov=+&rate=&lic=&licNum=&act=&actCat=actif&term=&startDate=&endDate='
+inspectiondata = []
 
 driver = webdriver.Chrome()
 driver.get(url)
 
 WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="tbodyMain"]/tr[1]/td[2]/a')))
 
-driver.execute_script("""document.querySelector("select[name='wb-auto-4_length'] option").value="15";""")
+# Changes how many data row is visible on the page
+driver.execute_script("""document.querySelector("select[name='wb-auto-4_length'] option").value="15";""") #change value to 3000 for final scrape
 select = Select(driver.find_element(By.NAME, "wb-auto-4_length"))
 select.select_by_index(2)
 select.select_by_index(0)
 
 
-
+# Extracts table data
 table_id = driver.find_element(By.ID, 'tbodyMain')
 rows = table_id.find_elements(By.TAG_NAME, "tr") # get all of the rows in the table
 for row in rows:
@@ -31,17 +35,33 @@ for row in rows:
     links = row.find_elements(By.XPATH, './/td[2]/a')
     for link in links:
         company_link = link.get_attribute('href')
-    print(ref,company,company_link,site,ins_date,rating,clicense)
+    # print(ref,company,company_link,site,ins_date,rating,clicense)
 
-    # Open a new window
+    # Open a new window for each company data
     driver.execute_script("window.open('');")
     driver.switch_to.window(driver.window_handles[1])
     driver.get(company_link)
     WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="inspectionTable"]/tr[1]/td[1]')))
-    location = driver.find_element(By.ID, 'address')
-    print(location.text)
-
+    location = driver.find_element(By.ID, 'address').text
+    # print(location.text)
 
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
 
+
+    # Adds all data into dataframe for exporting
+    insp_data = {
+        'reference' : ref,
+        'company': company,
+        'site': site,
+        'inspection_date': ins_date,
+        'rating': rating,
+        'currently_licensed': clicense,
+        'address': location
+    }
+    inspectiondata.append(insp_data)
+
+# Exports to CSV
+df = pd.DataFrame(inspectiondata)
+print(df)
+df.to_csv('table.csv')
