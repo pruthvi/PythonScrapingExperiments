2ed5574ebc8d2327ce77369714db4dc1baa90eaf
Pruthvi <pruthvi@users.noreply.github.com>
2022-04-02 12:59:52 -0400

    Change Number of records

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 url = 'https://www.drug-inspections.canada.ca/gmp/searchResult-en.html?estName=&ref=&site=&eType=&prov=+&rate=&lic=&licNum=&act=&actCat=actif&term=&startDate=&endDate='
 
@@ -9,6 +10,13 @@ driver = webdriver.Chrome()
 driver.get(url)
 
 WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="tbodyMain"]/tr[1]/td[2]/a')))
+
+driver.execute_script("""document.querySelector("select[name='wb-auto-4_length'] option").value="15";""")
+select = Select(driver.find_element(By.NAME, "wb-auto-4_length"))
+select.select_by_index(2)
+select.select_by_index(0)
+
+
 
 table_id = driver.find_element(By.ID, 'tbodyMain')
 rows = table_id.find_elements(By.TAG_NAME, "tr") # get all of the rows in the table
